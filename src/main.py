8084bd7897bb5e10232ee8323571ab78c106be4a
Mahesh <msghutukade1@gmail.com>
2026-04-09 12:18:27 +0530

    added main.py in src

---
+++ src/main.py
@@ -0,0 +1,26 @@
+from src.preprocessing import preprocess_data
+from src.models import get_rf
+from src.train import train_model
+from src.evaluate import evaluate
+
+from sklearn.model_selection import train_test_split
+
+# preprocess
+X, y, scaler = preprocess_data("data/combine.csv")
+
+# split
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+# model
+rf = get_rf()
+
+# train
+rf = train_model(rf, X_train, y_train)
+
+# predict
+y_pred = rf.predict(X_test)
+
+# evaluate
+evaluate(y_test, y_pred)
