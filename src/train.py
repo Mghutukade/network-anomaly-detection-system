56c2322bf702d1235feded05556613414473a28b
Mahesh <msghutukade1@gmail.com>
2026-04-09 12:04:02 +0530

    Added training logic

--- src/train.py
+++ src/train.py
@@ -1,8 +1,4 @@
-# Hybrid model 
-final_pred = []
-
-for rf_p, iso_p in zip(rf_pred, y_pred_iso):
-    if rf_p == 1 or iso_p == 1:
-        final_pred.append(1)
-    else:
-        final_pred.append(0)
+def train_model(model, X_train, y_train):
+    model.fit(X_train, y_train)
+    print("Model trained ✅")
+    return model
