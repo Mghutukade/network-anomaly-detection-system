97ed92dc5c2c5dadec6b4bc318242ce6b8f2051e
Mahesh <msghutukade1@gmail.com>
2026-04-18 12:42:22 +0530

    train.py updated

--- src/train.py
+++ src/train.py
@@ -1,4 +1,15 @@
+import os
+import joblib
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
 def train_model(model, X_train, y_train):
     model.fit(X_train, y_train)
     print("Model trained ✅")
+
+    # Save model
+    model_path = os.path.join(BASE_DIR, "model.pkl")
+    joblib.dump(model, model_path)
+
+    print(f"Model saved at {model_path} ✅")
     return model
